2708a02aa69f32680ee5ab0926c677601e0a6631
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-06-20 10:01:31 -0400

    Calculated total amount in PyBank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,12 @@
+#import dependencies
 import os
 import csv
 
+#select a path for a file in Resources folder
 budget_csv = os.path.join('Resources','budget_data.csv')
 
 total_months = 0
+total_amount = 0
 
 with open(budget_csv) as budget_file:
     csvreader = csv.reader(budget_file, delimiter = ',')
@@ -13,12 +16,10 @@ with open(budget_csv) as budget_file:
     for row in csvreader:
         
         total_months += 1
-
         
+        profit_loss = int(row[1])
+        total_amount += profit_loss
 
-
-
-
-
+print(total_amount)
 print(total_months)
 
