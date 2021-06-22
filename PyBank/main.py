b1176bbfc3f1bc64fd85165bfbc00cc5c44cbf59
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-06-22 02:37:08 -0400

    Saving PyBank code. Yet to complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,12 +8,18 @@ budget_csv = os.path.join('Resources','budget_data.csv')
 total_months = 0
 total_amount = 0
 change = []
-month = []
+months = []
 
 with open(budget_csv) as budget_file:
     csvreader = csv.reader(budget_file, delimiter = ',')
 
     header = next(csvreader)
+    
+    first_row = next(csvreader)
+    total_months += 1
+    total_amount = int(first_row[1])
+
+    previous_amount = int(first_row[1])
 
     for row in csvreader:
         
@@ -21,14 +27,25 @@ with open(budget_csv) as budget_file:
         
         total_amount += int(row[1])
 
-        
-        
-        #for i in range(0,len(row[1])):
-         #   change.append
-          #  break
-            #difference = (profit_loss[i+1]-profit_loss[i])
-            #change.append(difference)
-#print(diff)
-#print(change)
+        change.append(int(row[1])-previous_amount)
+        months.append(row[0])
+
+        previous_amount = int(row[1])
+
+average = round(sum(change)/len(change),2)
+
+greatest_increase = max(change)
+greatest_decrease = min(change)
+
+index_max = change.index(max(change))
+
+print(index_max)
+
+
+
+
+print(greatest_decrease)
+print(greatest_increase)
+print("${:,.2f}".format(average))       
 print(total_amount)
-#print(total_months)
+print(total_months)
