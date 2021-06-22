3d3fc94ba3895fbf188e8d1f640f019afad47a86
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-06-21 21:13:13 -0400

    PyPoll code commented

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,36 +1,38 @@
 #import dependencies
 import os
 import csv
-#from collections import Counter
 
-#select a path for a file in Resources folder
+#select a path for a CSV file in Resources folder to read
 election_csv = os.path.join("Resources","election_data.csv")
 
+#Declaring variables
 total_votes = 0
-#candidate = []
 
 khan = 0
 correy = 0
 li = 0
 otooley = 0
+
 percent_khan = 0
 percent_correy = 0
 percent_li = 0
 percent_otooley = 0
 
+#Reading CSV file
 with open(election_csv) as election_file:
     csvreader = csv.reader(election_file, delimiter = ',')
 
+    #skipping header row
     header = next(csvreader)
 
+    #Looping through rows of CSV file
     for row in csvreader:
 
+        #Calculating total votes 
         total_votes += 1
         
-        #candidate.append(row[2])
-
-        #c = Counter(candidate)
-
+        #If loop to calculate total votes for each candidates
+        #increment candidate variable if it is equal to row value
         if row[2] == "Khan":
             khan += 1
         elif row[2] == "Correy":
@@ -40,36 +42,40 @@ with open(election_csv) as election_file:
         elif row[2] == "O'Tooley":
             otooley += 1
 
-percent_khan = round(((khan/total_votes)*100),2)
-percent_correy = round(((correy/total_votes)*100),2)
-percent_li = round(((li/total_votes)*100),2)
-percent_otooley = round(((otooley/total_votes)*100),2)
+    #Calculating percentage votes for each candidate
+    #Formatting number into percentage
+    percent_khan = "{:.3%}".format(khan/total_votes)
+    percent_correy = "{:.3%}".format(correy/total_votes)
+    percent_li = "{:.3%}".format(li/total_votes)
+    percent_otooley = "{:.3%}".format(otooley/total_votes)
 
-candidate = {khan:"Khan",correy:"Correy",li:"Li",otooley:"O'Tooley"}
+    #Dictionary to define key-value pair of total votes for each candidate with their name
+    candidate = {khan:"Khan",correy:"Correy",li:"Li",otooley:"O'Tooley"}
+    
+    #using max() function to find the highest value of key in the dictionary
+    #using get() function to get value for the highest key in a variable
+    #this will determine our winner
+    winner = candidate.get(max(candidate))
 
-#candidate1 = {"Khan":khan,"Correy":correy,"Li":li,"O'Tooley":otooley}
-
-#print(candidate)
-
-#win = max(candidate)
-
-winner = candidate.get(max(candidate))
-
-election_result = (f"Election Results\n"
+#Entering print statements into a variable 
+election_result = ("Election Results\n"
                     "----------------------\n"
-                    f"Total Votes: {total_votes}\n"
+                    "Total Votes: {total_votes}\n"
                     "----------------------\n"
-                    f"Khan: {percent_khan:.3f}% ({khan})\n"
-                    f"Correy: {percent_correy:.3f}% ({correy})\n"
-                    f"Li: {percent_li:.3f}% ({li})\n"
-                    f"O'Tooley: {percent_otooley:.3f}% ({otooley})\n"
+                    f"Khan: {percent_khan} ({khan})\n"
+                    f"Correy: {percent_correy} ({correy})\n"
+                    f"Li: {percent_li} ({li})\n"
+                    f"O'Tooley: {percent_otooley} ({otooley})\n"
                     "----------------------\n"
                     f"Winner: {winner}\n"
                     "----------------------")
 
+#printing result on terminal
 print(election_result)
 
+#select a path to store output in an Analysis folder
 output_path = os.path.join("Analysis","PyPoll_Analysis.txt")
 
+#Writing output in a Text file
 with open(output_path, "w") as txtfile:
     txtfile.write(election_result)
